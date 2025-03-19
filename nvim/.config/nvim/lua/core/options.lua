4e9deb203f3b2fd3f0e6656a50e19f002143fbc3
jesichi/linux <jesichiko@gmail.com>
2025-03-19 17:26:33 -0600

    New config options

--- nvim/.config/nvim/lua/core/options.lua
+++ nvim/.config/nvim/lua/core/options.lua
@@ -1,11 +1,35 @@
 local opt = vim.opt
 
 -- indentation
-opt.tabstop = 2
-opt.softtabstop = 2
-opt.shiftwidth = 2
+opt.tabstop = 4
+opt.softtabstop = 4
+opt.shiftwidth = 4
 opt.expandtab = true
 opt.smartindent = true
 
---colorscheme
+-- relative number
+opt.number = true
+opt.relativenumber = true
+
+-- split
+opt.splitright = true
+
+-- wrap
+opt.wrap = false
+
+-- clipboard
+opt.clipboard = "unnamedplus"
+
+-- scrolling
+opt.scrolloff = 999
+
+opt.virtualedit = "block"
+
+-- replace split
+opt.inccommand = "split"
+
+opt.ignorecase = true
+
+-- colorscheme
 vim.cmd("colorscheme catppuccin-macchiato")
+
--- nvim/.config/nvim/lua/plugins/lsp/mason-lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp/mason-lsp.lua
@@ -1,46 +1,46 @@
 return {
-	--mason
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	--mason-lspconfig
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				automatic_installation = true,
-			})
-		end,
-	},
-	--lsp
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-			local lspconfig = require("lspconfig")
-			local mason_lspconfig = require("mason-lspconfig")
+  --mason
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  --mason-lspconfig
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        automatic_installation = true,
+      })
+    end,
+  },
+  --lsp
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      local lspconfig = require("lspconfig")
+      local mason_lspconfig = require("mason-lspconfig")
 
-			for _, server in ipairs(mason_lspconfig.get_installed_servers()) do
-				lspconfig[server].setup({
-					capabilities = capabilities,
-				})
-			end
+      for _, server in ipairs(mason_lspconfig.get_installed_servers()) do
+        lspconfig[server].setup({
+          capabilities = capabilities,
+        })
+      end
 
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-				settings = {
-					Lua = {
-						workspace = {
-							library = {
-								"${3rd}/love2d/library",
-							},
-						},
-					},
-				},
-			})
-		end,
-	},
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities,
+        settings = {
+          Lua = {
+            workspace = {
+              library = {
+                "${3rd}/love2d/library",
+              },
+            },
+          },
+        },
+      })
+    end,
+  },
 }
