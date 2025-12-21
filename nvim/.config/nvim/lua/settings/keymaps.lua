b4e42798cbff7f271613232f97891321cdbe14fc
jesichi/linux <jesichiko@gmail.com>
2025-12-21 16:50:00 -0600

    refactoring

--- nvim/.config/nvim/lua/settings/keymaps.lua
+++ nvim/.config/nvim/lua/settings/keymaps.lua
@@ -1,16 +1,22 @@
+-- Helper autocmd
+vim.api.nvim_create_autocmd({ "BufLeave", "InsertEnter", "WinLeave" }, {
+	callback = function()
+		vim.lsp.buf.clear_references()
+	end,
+})
+
 -- @Keymaps
 
 local opts = { noremap = true, silent = true }
-
 -- <leader> = "<space>" -> lazy.lua
 
 -- @Search:
 -- Telescope
 local builtin = require("telescope.builtin")
-
 vim.keymap.set("n", "<C-p>", builtin.find_files, opts)
 vim.keymap.set("n", "<C-o>", builtin.oldfiles, opts)
 vim.keymap.set("n", "<C-l>", builtin.live_grep, opts)
+vim.keymap.set("n", "<leader>fh", builtin.help_tags, opts)
 
 -- Oil
 vim.keymap.set("n", "<C-ñ>", "<cmd>Oil<CR>", opts)
@@ -19,7 +25,8 @@ vim.keymap.set("n", "<C-ñ>", "<cmd>Oil<CR>", opts)
 -- @LSP
 vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
 vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, opts)
-vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+vim.keymap.set("n", "<leader>re", vim.lsp.buf.rename, opts)
+vim.keymap.set("n", "<leader>gr", vim.lsp.buf.document_highlight, opts)
 vim.keymap.set("n", "<leader>gf", function()
 	require("conform").format({ lsp_format = "fallback" })
 end, opts)
@@ -43,4 +50,8 @@ vim.keymap.set("n", "<leader>l", "<cmd>Lazy<CR>", opts)
 
 -- @Terminal
 -- vim Builtin Terminal
-vim.keymap.set("n", "<leader>ñ", "<cmd> vertical terminal <CR>", opts)
+vim.keymap.set("n", "<leader>ñ", function()
+	vim.cmd.vsplit()
+	vim.cmd.term()
+	vim.api.nvim_win_set_width(0, 35)
+end, opts)
